d0ff0baaf3b3d04836e7b436a0d3962e10f8e78a
szlupek <diab149@vp.pl>
2018-05-07 13:45:14 +0200

    Magnetization, connections etc

--- VoterModel/src/pl/edu/pw/fizyka/voter/NodeList.java
+++ VoterModel/src/pl/edu/pw/fizyka/voter/NodeList.java
@@ -9,6 +9,9 @@ public class NodeList {
 
 	int mi, N;
 	double p;
+	int rhoP;
+	int rhoM;
+	double M;
 
 	public NodeList(int Mi, int NN, double P) {
 		mi = Mi;
@@ -20,21 +23,20 @@ public class NodeList {
 		for (int i = 0; i < N; ++i)
 			tmp.add(i);
 
-		//System.out.println(tmp.get(N - 1));
+		// System.out.println(tmp.get(N - 1));
 		for (int i = 0; i < N; ++i)
 			list.add(new Node());
-		//while (tmp.size() > 0) {
-		for(int x=0;x<4;++x){
+		// while (tmp.size() > 0) {
+		for (int x = 0; x < 4; ++x) {
 			for (int j = 0; j < N; ++j) {
-				if(list.get(j).connections.size() < mi) {
+				if (list.get(j).connections.size() < mi) {
 					int rIndex = j;
 					while (rIndex == j || list.get(j).connections.contains(rIndex))
-						rIndex=tmp.get((new Random()).nextInt(tmp.size()));
-					//System.out.println(j+" "+rIndex+" "+list.get(j).connections.size()+" "+tmp );
-					
+						rIndex = tmp.get((new Random()).nextInt(tmp.size()));
+					// System.out.println(j+" "+rIndex+" "+list.get(j).connections.size()+" "+tmp );
+
 					list.get(j).connections.add(rIndex);
 					list.get(rIndex).connections.add(j);
-					
 
 					if (list.get(rIndex).connections.size() >= mi)
 						tmp.remove(tmp.indexOf(rIndex));
@@ -42,32 +44,91 @@ public class NodeList {
 					if (list.get(j).connections.size() >= mi)
 						tmp.remove(tmp.indexOf(j));
 				}
-				//tmp.remove(tmp.indexOf(j));
+				// tmp.remove(tmp.indexOf(j));
 			}
-	}
-		//}
+		}
+		// }
 
 	}
-	
-	
-	
-	public void nextTimeStep(){
-		Random rgen=new Random();
-		for(Node n:list){
-			int j=rgen.nextInt(mi);
-			int jIndex=n.connections.get(j);
-			if(n.s!=list.get(jIndex).s){
-				if(rgen.nextDouble()<p){
-					int aIndex=rgen.nextInt(N);
-					while(n.s!=list.get(aIndex).s||!n.connections.contains(aIndex))aIndex=rgen.nextInt(N);
+
+	public void nextTimeStep() {
+		Random rgen = new Random();
+		for (Node n : list) {
+			int j = rgen.nextInt(mi);
+			int jIndex = n.connections.get(j);
+			if (n.s != list.get(jIndex).s) {
+				if (rgen.nextDouble() < p) {
+					int aIndex = rgen.nextInt(N);
+					while (n.s != list.get(aIndex).s || !n.connections.contains(aIndex))
+						aIndex = rgen.nextInt(N);
 					n.connections.set(j, aIndex);
-				}
-				else{
-					n.s*=-1;
+				} else {
+					n.s *= -1;
 				}
 			}
 		}
-		
+
 	}
 
+	public void calcM() {
+		double sum = 0;
+		for (int ii = 0; ii < N; ii++)
+			sum += list.get(ii).s;
+		M = sum / N;
+	}
+
+	public void printM() {
+		System.out.println(M);
+	}
+
+	public double getM() {
+		return M;
+	}
+
+	public void calcRho() {
+		int tempRhoP = 0;
+		int tempRhoM = 0;
+		for (int ii = 0; ii < N; ii++) {
+			for (int n : list.get(ii).connections) {
+				if (list.get(ii).s == 1 && list.get(n).s == 1) {
+					tempRhoP++;
+				} else if (list.get(ii).s == -1 && list.get(n).s == -1) {
+					tempRhoM++;
+				} else {
+				}
+			}
+		}
+		rhoM = tempRhoM / 2;
+		rhoP = tempRhoP / 2;
+	}
+
+	public int getRhoM() {
+		return rhoM;
+	}
+
+	public int getRhoP() {
+		return rhoP;
+	}
+
+	public void printRhoP() {
+		System.out.println(rhoP);
+	}
+
+	public void printRhoM() {
+		System.out.println(rhoM);
+	}
+
+	public int getNumberOfConnections() {
+		return N * mi/2;
+	}
+
+	public void printNumberOfConnections() {
+		System.out.println(N *mi/2);
+	}
+
+	public void printAllConnections() {
+		for (int ii = 0; ii < N; ii++) {
+			System.out.println(list.get(ii).connections);
+		}
+	}
 }
--- VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
+++ VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
@@ -6,11 +6,21 @@ public class Simulation {
 		NodeList test=new NodeList(3,1000,0.5);
 		int i=0;
 		for(Node n:test.list){System.out.println(i+" "+n.s+"  "+n.connections);++i;}
-		
+
+	}
+	public void parameterTest(){
+		NodeList test=new NodeList(3,1000,0.5);
+		test.calcRho();
+		test.calcM();
+		test.printAllConnections();
+		test.printRhoM();
+		test.printRhoP();
+		test.printM();
+		test.printNumberOfConnections();
 	}
 	
 	public Simulation(){
-		//this.NodeListGenerationTest();
+		this.parameterTest();
 	}
 
 }
