f3752c4ba04d58a0bc99de3196106f24884c9a98
szlupek <diab149@vp.pl>
2018-05-21 00:25:35 +0200

    nextStep i poprawki w danych

--- VoterModel/src/pl/edu/pw/fizyka/voter/NodeList.java
+++ VoterModel/src/pl/edu/pw/fizyka/voter/NodeList.java
@@ -9,9 +9,9 @@ public class NodeList {
 
 	int mi, N;
 	double p;
-	int rhoP;
-	int rhoM;
-	int rho;
+	double rhoP;
+	double rhoM;
+	double rho;
 	double M;
 
 	public NodeList(int Mi, int NN, double P) {
@@ -49,52 +49,64 @@ public class NodeList {
 			}
 		}
 		// }
-	//	System.out.println("Gen OK");
+		// System.out.println("Gen OK");
 	}
 
 	public void nextTimeStep() {
 
 		Random rgen = new Random();
-		int iIndex=rgen.nextInt(N);
-		Node n=list.get(iIndex);
-		int conn=n.connections.size();
-		if(conn>0){	
-			int j = rgen.nextInt(conn>0?conn:mi);
+		int iIndex = rgen.nextInt(N);
+		Node n = list.get(iIndex);
+		// System.out.println("mój Index- " + iIndex);
+		int conn = n.connections.size();
+		// System.out.println("conn- " + conn);
+		if (conn > 0) {
+			int j = rgen.nextInt(conn > 0 ? conn : mi);
 			int jIndex = n.connections.get(j);
+			// System.out.println("j- " + j + " jIndex- " + jIndex);
 			if (n.s != list.get(jIndex).s) {
-				if (rgen.nextDouble() < p) {
+
+				double por = rgen.nextDouble();
+				// System.out.println(por + ", p - " + p);
+				if (por < p) {
 					int aIndex = rgen.nextInt(N);
-					
-					////error find better way//probably fixed
-					int switcher=0;
-					ArrayList<Integer> aList=new ArrayList<Integer>();
-					//limit 100
-					while (switcher<100&&(n.s != list.get(aIndex).s || n.connections.contains(aIndex)||aIndex==iIndex)){
+
+					//// error find better way//probably fixed
+					int switcher = 0;
+					ArrayList<Integer> aList = new ArrayList<Integer>();
+					// limit 100
+					while (switcher < 100
+							&& (n.s != list.get(aIndex).s || n.connections.contains(aIndex) || aIndex == iIndex)) {
 						switcher++;
 						aIndex = rgen.nextInt(N);
+						// System.out.println("jestem");
 					}
-					if(switcher==100){
-						for(Node nd:list)
-							if(n.s==nd.s&&list.indexOf(n)!=list.indexOf(nd)&&!n.connections.contains(aIndex))
+					if (!((n.s != list.get(aIndex).s || n.connections.contains(aIndex) || aIndex == iIndex)))
+						aList.add(aIndex);
+					if (switcher == 100) {
+						for (Node nd : list)
+							if (n.s == nd.s && list.indexOf(n) != list.indexOf(nd) && !n.connections.contains(aIndex))
 								aList.add(list.indexOf(nd));
-						if(aList.size()>0)aIndex= aList.get(rgen.nextInt(aList.size()));
-			//			System.out.println("Switcheru"+aList);
+						if (aList.size() > 0)
+							aIndex = aList.get(rgen.nextInt(aList.size()));
+						// System.out.println("Switcheru"+aList);
 					}
-					//System.out.println(list.indexOf(n)+" "+aIndex+" "+jIndex);
-					if(aList.size()>0){
+					// System.out.println(list.indexOf(n)+" "+aIndex+" "+jIndex);
+					if (aList.size() > 0) {
 						n.connections.set(j, aIndex);
 						list.get(aIndex).connections.add(iIndex);
 						list.get(jIndex).connections.remove(list.get(jIndex).connections.indexOf(iIndex));
+						// System.out.println("foch");
 					}
 				} else {
 					n.s *= -1;
+					// System.out.println("zdrada");
 				}
-				
+
 			}
+			// else {System.out.println("nic");}
 		}
 	}
-
-	
 
 	public double calcM() {
 		double sum = 0;
@@ -112,10 +124,10 @@ public class NodeList {
 		return M;
 	}
 
-	public int calcRho() {
-		int tempRhoP = 0;
-		int tempRhoM = 0;
-		int tmpRho=0;
+	public double calcRho() {
+		double tempRhoP = 0;
+		double tempRhoM = 0;
+		double tmpRho = 0;
 		for (int ii = 0; ii < N; ii++) {
 			for (int n : list.get(ii).connections) {
 				if (list.get(ii).s == 1 && list.get(n).s == 1) {
@@ -127,17 +139,17 @@ public class NodeList {
 				}
 			}
 		}
-		rhoM = tempRhoM / 2;
-		rhoP = tempRhoP / 2;
-		rho=tmpRho/2;
+		rhoM = tempRhoM / mi /N ;
+		rhoP = tempRhoP / mi /N;
+		rho = tmpRho / mi /N;
 		return rho;
 	}
 
-	public int getRhoM() {
+	public double getRhoM() {
 		return rhoM;
 	}
 
-	public int getRhoP() {
+	public double getRhoP() {
 		return rhoP;
 	}
 
@@ -150,28 +162,26 @@ public class NodeList {
 	}
 
 	public int getNumberOfConnections() {
-		return N * mi/2;
+		return N * mi / 2;
 	}
 
 	public void printNumberOfConnections() {
-		System.out.println(N *mi/2);
+		System.out.println(N * mi / 2);
 	}
 
 	public void printAllConnections() {
 		for (int ii = 0; ii < N; ii++) {
-			System.out.println(ii+" "+list.get(ii).s+" "+list.get(ii).connections);
+			System.out.println(ii + " " + list.get(ii).s + " " + list.get(ii).connections);
 		}
 	}
-	
-	
-	public void printParm(){
+
+	public void printParm() {
 		this.calcRho();
 		this.calcM();
-		
+
 		this.printRhoM();
 		this.printRhoP();
 		this.printM();
 	}
-	
-	
-	}
+
+}
--- VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
+++ VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
@@ -7,15 +7,19 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Simulation {
-	
-	public void NodeListGenerationTest(){
-		NodeList test=new NodeList(3,1000,0.5);
-		int i=0;
-		for(Node n:test.list){System.out.println(i+" "+n.s+"  "+n.connections);++i;}
+
+	public void NodeListGenerationTest() {
+		NodeList test = new NodeList(3, 1000, 0.5);
+		int i = 0;
+		for (Node n : test.list) {
+			System.out.println(i + " " + n.s + "  " + n.connections);
+			++i;
+		}
 
 	}
-	public void parameterTest(){
-		NodeList test=new NodeList(3,1000,0.5);
+
+	public void parameterTest() {
+		NodeList test = new NodeList(3, 1000, 0.5);
 		test.calcRho();
 		test.calcM();
 		test.printAllConnections();
@@ -24,106 +28,125 @@ public class Simulation {
 		test.printM();
 		test.printNumberOfConnections();
 	}
-	
-	public void nextStepTest(){
-		NodeList test=new NodeList(3,10,0.5);
+
+	public void nextStepTest() {
+		NodeList test = new NodeList(3, 10, 0.5);
 		System.out.println("OK");
 		test.printAllConnections();
+		test.calcRho();
+		test.printRhoM();
 		test.nextTimeStep();
 		System.out.println("*****");
+		test.calcRho();
+		test.printRhoM();
 		test.printAllConnections();
 		test.nextTimeStep();
 		System.out.println("*****");
+		test.calcRho();
+		test.printRhoM();
 		test.printAllConnections();
-		
+		test.nextTimeStep();
+		System.out.println("*****");
+		test.calcRho();
+		test.printRhoM();
+		test.printAllConnections();
+		test.nextTimeStep();
+		System.out.println("*****");
+		test.calcRho();
+		test.printRhoM();
+		test.printAllConnections();
+
+
 	}
-	
-	
-	public void evolutionTest1(){
-		NodeList test=new NodeList(4,10000,0);
+
+	public void evolutionTest1() {
+		NodeList test = new NodeList(4, 1000, 0);
 		System.out.println("OK");
 		test.printParm();
-		for(int i=0;i<200000&&test.M*test.M!=1;++i){
+		for (int i = 0; i < 200000 && test.M * test.M != 1; ++i) {
 			test.nextTimeStep();
-			
-			if(i%1000==0){System.out.println("*****************");test.printParm();}
+
+			if (i % 1000 == 0) {
+				System.out.println("*****************");
+				test.printParm();
+			}
 		}
-		int conn=0;
-		for(Node n:test.list){
-			conn+=n.connections.size();
+		int conn = 0;
+		for (Node n : test.list) {
+			conn += n.connections.size();
 		}
-		System.out.println("Total num of conn: "+conn/2);
+		System.out.println("Total num of conn: " + conn / 2);
 	}
-	
-	public void dataGen(){
-		
+
+	public void dataGen() {
+
 		PrintWriter writer;
 		PrintWriter writer2;
-		int N=500;
-		int mi=4;
+		int N = 500;
+		int mi = 4;
 		DecimalFormat df = new DecimalFormat("0.000");
 		DecimalFormat df2 = new DecimalFormat("0.00");
 		try {
-			writer = new PrintWriter("13_data_N"+N+"_Mi"+mi+".txt", "UTF-8");
-			
+			writer = new PrintWriter("13_data_N" + N + "_Mi" + mi + ".txt", "UTF-8");
+
 			writer.println("p rhoP rhoM M rho");
-			for(double i=0.2;i<=0.61;i+=0.1){
-				NodeList test=new NodeList(mi,N,i);
+			for (double i = 0.2; i <= 0.61; i += 0.1) {
+				NodeList test = new NodeList(mi, N, i);
 				System.out.println("ok");
-				
-				writer2= new PrintWriter("rho/dataAVG_N"+N+"_p"+df2.format(i));
-				
-				double rho=test.calcRho();
-				
-				ArrayList<Double> rhoList=new ArrayList<Double>();
-				int j=0;
-				int avg=0;
-				for(;avg<10;++avg) {
-					test=new NodeList(mi,N,i);
-					
-					j=0;
-					rho=test.calcRho();
-					
-					System.out.println("ok "+avg);
-				while(rho>0){
-	//				writer2.println(j+" "+rho);
-	//				System.out.println(test.calcRho()+" "+test.M);
-					if(rhoList.size()==j)rhoList.add(rho);
-					else rhoList.set(j, rhoList.get(j)+rho);
-					test.nextTimeStep();
-					rho=test.calcRho();
-					j++;
-				}
-				System.out.println("j "+j);
+
+				writer2 = new PrintWriter("rho/dataAVG_N" + N + "_p" + df2.format(i));
+
+				double rho = test.calcRho();
+
+				ArrayList<Double> rhoList = new ArrayList<Double>();
+				int j = 0;
+				int avg = 0;
+				for (; avg < 10; ++avg) {
+					test = new NodeList(mi, N, i);
+
+					j = 0;
+					rho = test.calcRho();
+
+					System.out.println("ok " + avg);
+					while (rho > 0) {
+						// writer2.println(j+" "+rho);
+						// System.out.println(test.calcRho()+" "+test.M);
+						if (rhoList.size() == j)
+							rhoList.add(rho);
+						else
+							rhoList.set(j, rhoList.get(j) + rho);
+						test.nextTimeStep();
+						rho = test.calcRho();
+						j++;
+					}
+					System.out.println("j " + j);
 				}
 				test.calcRho();
 				test.calcM();
-				
-				for(int x=0;x<rhoList.size();x++)
-					writer2.println(x+" "+rhoList.get(x)/avg);
+
+				for (int x = 0; x < rhoList.size(); x++)
+					writer2.println(x + " " + rhoList.get(x) / avg);
 				writer2.flush();
 				writer2.close();
-				
-				String str=df.format(i)+" "+test.getRhoP()+" "+test.getRhoM()+" "+test.getM()+"   "+test.rho;
+
+				String str = df.format(i) + " " + test.getRhoP() + " " + test.getRhoM() + " " + test.getM() + "   "
+						+ test.rho;
 				writer.println(str.replaceAll("\\.", ","));
 				writer.flush();
-				System.out.println("Iteracja p="+i);
+				System.out.println("Iteracja p=" + i);
 			}
 			writer.close();
-			
+
 		} catch (FileNotFoundException | UnsupportedEncodingException e) {
 			e.printStackTrace();
-		}	
 		}
-		
-		
-	
-	
-	public Simulation(){
-		//this.parameterTest();
+	}
+
+	public Simulation() {
+		// this.parameterTest();
 		//this.nextStepTest();
-		//this.evolutionTest1();
-		this.dataGen();
+		// this.evolutionTest1();
+		 this.dataGen();
 	}
 
 }
