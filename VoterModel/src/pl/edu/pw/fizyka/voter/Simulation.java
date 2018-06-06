8a8d2d2641350bf7e24de182ba696024007d6a9d
fisher <mkpubliczny@gmail.com>
2018-06-06 20:09:22 +0200

    Merge needed

--- VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
+++ VoterModel/src/pl/edu/pw/fizyka/voter/Simulation.java
@@ -81,19 +81,19 @@ public class Simulation {
 
 		PrintWriter writer;
 		PrintWriter writer2;
-		int N = 500;
+		int N = 2500;
 		int mi = 4;
 		DecimalFormat df = new DecimalFormat("0.000");
 		DecimalFormat df2 = new DecimalFormat("0.00");
 		try {
-			writer = new PrintWriter("13_data_N" + N + "_Mi" + mi + ".txt", "UTF-8");
+			writer = new PrintWriter("50_data_N" + N/1000 + "k_Mi" + mi + ".txt", "UTF-8");
 
 			writer.println("p rhoP rhoM M rho");
 			for (double i = 0.2; i <= 0.61; i += 0.2) {
 				NodeList test = new NodeList(mi, N, i);
 				System.out.println("ok");
 
-				writer2 = new PrintWriter("rho/dataAVG_N" + N + "_p" + df2.format(i));
+				writer2 = new PrintWriter("rho/2dataAVG_N" + N/1000 + "k_p" + df2.format(i));
 
 				double rho = test.calcRho();
 
@@ -101,26 +101,32 @@ public class Simulation {
 				ArrayList<Double> mList = new ArrayList<Double>();
 				int j = 0;
 				int avg = 0;
-				for (; avg < 1; ++avg) {
+				for (; avg < 10; ++avg) {
 					test = new NodeList(mi, N, i);
 
 					j = 0;
 					rho = test.calcRho();
 					double m = test.calcM();
 					System.out.println("ok " + avg);
-					while (rho > 0) {
+					//&&j/N<N optimization rho > 0
+					while (rho>0) {
+						test.nextTimeStep();
+						if(j%N==0) {
 						// writer2.println(j+" "+rho);
 						// System.out.println(test.calcRho()+" "+test.M);
-						if (rhoList.size() == j) {
+						if (rhoList.size() == j/N) {
 							rhoList.add(rho);
 							mList.add(m);
 						} else {
-							rhoList.set(j, rhoList.get(j) + rho);
-							mList.set(j, mList.get(j) + m);
+							rhoList.set(j/N, rhoList.get(j/N) + rho);
+							mList.set(j/N, mList.get(j/N) + m);
 						}
-						test.nextTimeStep();
-						rho = test.calcRho();
-						m = test.calcM();
+						
+						//	System.out.println(j/N+" krok");
+							rho = test.calcRho();
+							m = test.calcM();
+						}
+						
 						j++;
 					}
 					System.out.println("j " + j);
@@ -145,12 +151,69 @@ public class Simulation {
 			e.printStackTrace();
 		}
 	}
+	void absM_test() {
+		PrintWriter writer;
+		
+		int N = 2500;
+		int mi = 4;
+		int st=10;
+		DecimalFormat df = new DecimalFormat("0.000");
+		
+		try {
+			writer = new PrintWriter("100_data_N" + N/1000 + "k_Mi" + mi + ".txt", "UTF-8");
+			double mList[][]= new double[st][81];
+			//for(int x=0;x<st;x++)mList[x]=new double[];
+			writer.println("p M");
+			
+			for(int avg=0;avg<st;avg++) {
+			int jj=0;
+			for (double i = 0.2; i <= 0.61; i += 0.2) {
+				NodeList test = new NodeList(mi, N, i);
+				System.out.println("ok "+df.format(i)+" round "+avg);
 
+				double rho = test.calcRho();
+				//&&j/N<N optimization rho > 0
+				int j=0;
+				while (rho>0) {
+					test.nextTimeStep();
+					if(j%(N)==0)rho = test.calcRho();
+					j++;
+				}
+				test.calcRho();
+				mList[avg][jj]=test.calcM();	
+				jj++;
+			}
+			}
+			System.out.println("ok1");
+				ArrayList<Double> avgTab= new ArrayList<Double>();
+				for(int k=0;k<mList[0].length;k++)avgTab.add(Math.abs(mList[0][k]));
+				for(int i=1;i<st;++i)
+					for(int k=0;k<mList[i].length;k++)
+						avgTab.set(k,avgTab.get(k)+Math.abs(mList[i][k]));
+				System.out.println("ok2");
+				double p=0.28;
+				for(int i=0;i<avgTab.size();i++) {
+					
+					String str = df.format(p) + " " +avgTab.get(i)/st;
+					writer.println(str.replaceAll("\\.", ","));
+					writer.flush();
+					p+=0.001;
+				}
+
+			writer.close();
+
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 	public Simulation() {
 		// this.parameterTest();
 		// this.nextStepTest();
 		// this.evolutionTest1();
 		this.dataGen();
+		//this.absM_test();
 	}
 
 }
